7a3a26fedd8942eb4a659d43d0d06f933f29896a
giuliamenca07 <giuliamenca07@gmail.com>
2025-11-28 11:05:34 +0100

    secondo upload

--- src/Auto.java
+++ src/Auto.java
@@ -1,3 +1,24 @@
-public class Auto implements runnable {
+public class Auto implements Runnable {
 
+  private int numero;
+  private double velocita;
+  private double distanzaPercorsa;
+  private boolean inBox;
+  private Giudice podio;
+  private double distanzaGara;
+
+    public Auto(int numero, double velocita, double distanzaGara, Giudice podio) {
+        this.numero = numero;
+        this.velocita = velocita;
+        this.distanzaGara = distanzaGara;
+        this.podio = podio;
+    }
+
+    public void run() {
+
+    }
+    public void pitStop() {
+
+    }
 }
+
